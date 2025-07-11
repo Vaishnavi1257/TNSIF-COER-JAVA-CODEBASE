1edf89b40cf5e7c56d644d51a5767c9981af76a4
HP <HP@LAPTOP-NQ4M4D91>
2025-07-11 22:01:21 +0530

    Day2

--- Day2/src/tnsif/day2/Controlstatement.java
+++ Day2/src/tnsif/day2/Controlstatement.java
@@ -2,15 +2,13 @@ package tnsif.day2;
 
 public class Controlstatement {
 
-	public static void main(String[] args) {
-		int x=9;
-		if(x == 4) 
-			System.out.println("the value of x is 4");
-		else
-			System.out.println("the value of x is not 4");
-		
-		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			int x=9;
+			if(x == 4) 
+				System.out.println("the value of x is 4");
+			else
+				System.out.println("the value of x is not 4");
+			
 
-	}
-
+		}
 }
