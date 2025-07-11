7c5dff752f93f23ef8c9da9de972a087fe841edf
HP <HP@LAPTOP-NQ4M4D91>
2025-07-11 21:43:25 +0530

    Day13

---
+++ Day13/src/Synchronization/Bank.java
@@ -0,0 +1,11 @@
+package Synchronization;
+
+public class Bank {
+
+	static final int MINBAl=5000;
+	static final int DAILY_LIMIT=25000;
+	void deposit (int amt) throws DepositLimitException;
+	void withdraw(int amt) throws InsufficientBalanceException;
+	
+
+}
